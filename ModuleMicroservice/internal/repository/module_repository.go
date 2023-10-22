b9d088604b0fff693a87ba7e2feee85ef49d166a
Merlijn Busch <merlijn1busch@gmail.com>
2023-10-22 22:56:58 +0200

    updated repo where it does all crud towards a mongodb

--- ModuleMicroservice/internal/repository/module_repository.go
+++ ModuleMicroservice/internal/repository/module_repository.go
@@ -4,6 +4,7 @@ import (
 	"Module/graph/model"
 	"Module/internal/database"
 	"context"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"time"
 )
@@ -26,8 +27,6 @@ func (r *ModuleRepository) CreateModule(newModule *model.Module) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10) // 10-second timeout
 	defer cancel()
 
-	println("test")
-
 	_, err := r.collection.InsertOne(ctx, newModule)
 	if err != nil {
 		return err
@@ -36,34 +35,74 @@ func (r *ModuleRepository) CreateModule(newModule *model.Module) error {
 }
 
 func (r *ModuleRepository) UpdateModule(updatedModule *model.Module) error {
-	for i, module := range r.modules {
-		if module.ID == updatedModule.ID {
-			r.modules[i] = updatedModule
-			return nil
-		}
+	// Check if the module exists in MongoDB.
+	_, err := r.GetModuleByID(updatedModule.ID)
+	if err != nil {
+		return err // Return the error if it doesn't exist in MongoDB.
 	}
+
+	// If the module exists in MongoDB, update it in MongoDB.
+	filter := bson.M{"_id": updatedModule.ID}
+	update := bson.M{"$set": updatedModule}
+	_, err = r.collection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		return err // Return any MongoDB-related errors.
+	}
+
 	return nil
 }
 
 func (r *ModuleRepository) DeleteModuleByID(id string) error {
-	for i, module := range r.modules {
-		if module.ID == id {
-			r.modules = append(r.modules[:i], r.modules[i+1:]...)
-			return nil
-		}
+	// Check if the module exists in MongoDB.
+	_, err := r.GetModuleByID(id)
+	if err != nil {
+		return err // Return the error if it exists in MongoDB.
 	}
+
+	// If the module exists in MongoDB, delete it from MongoDB.
+	filter := bson.M{"_id": id}
+	_, err = r.collection.DeleteOne(context.TODO(), filter)
+	if err != nil {
+		return err // Return any MongoDB-related errors.
+	}
+
 	return nil
 }
 
 func (r *ModuleRepository) GetModuleByID(id string) (*model.Module, error) {
-	for _, module := range r.modules {
-		if module.ID == id {
-			return module, nil
-		}
+	// Attempt to fetch the module from MongoDB.
+	filter := bson.M{"_id": id}
+	var result model.Module
+	err := r.collection.FindOne(context.TODO(), filter).Decode(&result)
+	if err != nil {
+		return nil, err
 	}
-	return nil, nil
+	return &result, nil
 }
 
 func (r *ModuleRepository) ListModules() ([]*model.Module, error) {
-	return r.modules, nil
+	// Define an empty slice to store the modules.
+	var modules []*model.Module
+
+	// Retrieve all modules from MongoDB.
+	cursor, err := r.collection.Find(context.TODO(), bson.D{})
+	if err != nil {
+		return nil, err // Return any MongoDB-related errors.
+	}
+	defer func(cursor *mongo.Cursor, ctx context.Context) {
+		err := cursor.Close(ctx)
+		if err != nil {
+
+		}
+	}(cursor, context.TODO())
+
+	for cursor.Next(context.TODO()) {
+		var module model.Module
+		if err := cursor.Decode(&module); err != nil {
+			return nil, err // Return any decoding errors.
+		}
+		modules = append(modules, &module)
+	}
+
+	return modules, nil
 }
