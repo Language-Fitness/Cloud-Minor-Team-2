5bf51436653fa6066de648b66d329781e8d76fa9
BramTerlouw <bramterlouw12@gmail.com>
2023-10-24 21:53:20 +0200

    started on tests

--- ModuleMicroservice/test/internal/service/module_service_test.go
+++ ModuleMicroservice/test/internal/service/module_service_test.go
@@ -1,1 +1,47 @@
 package service
+
+import (
+	"testing"
+)
+
+//type MockRepository struct {
+//	mock.Mock
+//}
+//
+//type MockValidator struct {
+//	mock.Mock
+//}
+//func (m *MockValidator) GetErrors() []string {
+//	args := m.Called()
+//	return args.Get(0).([]string)
+//}
+
+func TestService_CreateModule(t *testing.T) {
+	//service := &ModuleService{}
+	//mockRepo := new(MockRepository)
+	//mockValidator := new(MockValidator)
+}
+
+func TestService_UpdateModule(t *testing.T) {
+	//service := &ModuleService{}
+	//mockRepo := new(MockRepository)
+	//mockValidator := new(MockValidator)
+}
+
+func TestService_DeleteModule(t *testing.T) {
+	//service := &ModuleService{}
+	//mockRepo := new(MockRepository)
+	//mockValidator := new(MockValidator)
+}
+
+func TestService_GetModuleById(t *testing.T) {
+	//service := &ModuleService{}
+	//mockRepo := new(MockRepository)
+	//mockValidator := new(MockValidator)
+}
+
+func TestService_ListModules(t *testing.T) {
+	//service := &ModuleService{}
+	//mockRepo := new(MockRepository)
+	//mockValidator := new(MockValidator)
+}
