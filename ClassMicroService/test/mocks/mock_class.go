4be9043653f38a00f244f9ca06a2b2173cd8488b
BramTerlouw <bramterlouw12@gmail.com>
2023-11-16 16:05:09 +0100

    repo tests work

--- ClassMicroService/test/mocks/mock_class.go
+++ ClassMicroService/test/mocks/mock_class.go
@@ -1,17 +1,47 @@
 package mocks
 
-import "example/graph/model"
+import (
+	"example/graph/model"
+	"time"
+)
 
-var MockNewClass = model.ClassInput{
-	Name: "Test Class",
+var Description = "This is a sample class."
+var UpdatedDescription = "This is an updated sample class."
+var Difficulty = 1
+var Timestamp = time.Now().String()
+var SoftDeleted = false
+
+var MockCreateInput = model.ClassInput{
+	ModuleID:    "module-id",
+	Name:        "Sample Class",
+	Description: &Description,
+	Difficulty:  &Difficulty,
+}
+
+var MockUpdateInput = model.ClassInput{
+	ModuleID:    "module-id",
+	Name:        "Sample Class",
+	Description: &UpdatedDescription,
+	Difficulty:  &Difficulty,
 }
 
 var MockClass = model.Class{
-	ID:   "3a3bd756-6353-4e29-8aba-5b3531bdb9ed",
-	Name: "Test Class",
+	ID:          "3a3bd756-6353-4e29-8aba-5b3531bdb9ed",
+	ModuleID:    "module-id",
+	Name:        "Sample Class",
+	Description: &Description,
+	Difficulty:  &Difficulty,
+	CreatedAt:   &Timestamp,
+	SoftDeleted: &SoftDeleted,
 }
 
-var MockUpdateClass = model.Class{
-	ID:   "3a3bd756-6353-4e29-8aba-5b3531bdb9ed",
-	Name: "Test Class",
+var MockUpdatedClass = model.Class{
+	ID:          "3a3bd756-6353-4e29-8aba-5b3531bdb9ed",
+	ModuleID:    "module-id",
+	Name:        "Sample Class",
+	Description: &UpdatedDescription,
+	Difficulty:  &Difficulty,
+	CreatedAt:   &Timestamp,
+	UpdatedAt:   &Timestamp,
+	SoftDeleted: &SoftDeleted,
 }
