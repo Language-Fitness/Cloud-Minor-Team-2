e0b2b06e882ec1482254d4bd5e0b3eb02caf0b3d
Bastiaan van der Bijl <666669@student.inholland.nl>
2023-11-28 16:49:23 +0100

    Hello

--- OpenAIMicroservice/App/Graphql/Mutations.py
+++ OpenAIMicroservice/App/Graphql/Mutations.py
@@ -6,10 +6,10 @@ class GenerateOpenAnswerQuestions(graphene.Mutation):
         subject = graphene.String(required=True)
         amount_questions = graphene.Int(required=True)
 
-    # The return fields for the mutation
+
     thread_id = graphene.String()
 
-    # The mutate method is called when the mutation is executed
+
     def mutate(self, info, subject, amount_questions):
         adapter = AssistantAPIAdapter()
         test = adapter.Generate_Multiple_Choice_Questions(subject, amount_questions)
@@ -23,16 +23,14 @@ class GenerateMultipleChoiceQuestions(graphene.Mutation):
         subject = graphene.String(required=True)
         amount_questions = graphene.Int(required=True)
 
-    # The return fields for the mutation
+
     thread_id = graphene.String()
 
-    # The mutate method is called when the mutation is executed
     def mutate(self, info, subject, amount_questions):
 
         adapter = AssistantAPIAdapter()
         test = adapter.Generate_Multiple_Choice_Questions(subject, amount_questions)
 
-        # Return an instance of this mutation
         return GenerateMultipleChoiceQuestions(thread_id=test)
 
 class GenerateExplanation(graphene.Mutation):
@@ -40,10 +38,10 @@ class GenerateExplanation(graphene.Mutation):
         question = graphene.String(required=True)
         given_answer = graphene.String(required=True)
 
-    # The return fields for the mutation
+
     thread_id = graphene.String()
 
-    # The mutate method is called when the mutation is executed
+
     def mutate(self, info, question, given_answer):
         adapter = AssistantAPIAdapter()
         test = adapter.Generate_Explanation(question, given_answer)
@@ -55,10 +53,9 @@ class GenerateAnswer(graphene.Mutation):
         question = graphene.String(required=True)
         question_info = graphene.String(required=True)
 
-    # The return fields for the mutation
+
     thread_id = graphene.String()
 
-    # The mutate method is called when the mutation is executed
     def mutate(self, info, question, question_info):
         adapter = AssistantAPIAdapter()
         test = adapter.Generate_Answer(question, question_info)
@@ -66,7 +63,6 @@ class GenerateAnswer(graphene.Mutation):
         return GenerateExplanation(thread_id=test)
 
 
-# The ObjectType for mutations should reference the mutation classes without instantiation
 class Mutation(graphene.ObjectType):
     generate_explanation = GenerateExplanation.Field()
     generate_open_answer_questions = GenerateOpenAnswerQuestions.Field()
--- OpenAIMicroservice/App/Graphql/Queries.py
+++ OpenAIMicroservice/App/Graphql/Queries.py
@@ -5,8 +5,7 @@ class Query(ObjectType):
     retrieve_response = Field(String, thread_id=String(required=True))
 
     async def resolve_retrieve_response(self, info, thread_id):
-        # Your logic to retrieve the response goes here.
-        # This is just a placeholder. Replace it with a call to your actual data retrieval function.
+
         adapter = AssistantAPIAdapter()
-        explanation = adapter.Retrieve_Response(thread_id)
-        return explanation
+        response = adapter.Retrieve_Response(thread_id)
+        return response
