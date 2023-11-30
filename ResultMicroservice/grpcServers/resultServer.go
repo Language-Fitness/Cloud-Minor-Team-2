72aef5c67afd5e336ed91fb309b5b46bc6a44f43
Jaimy Monsuur <jaimymonsuur@gmail.com>
2023-11-30 18:06:09 +0100

    updated grpc code related to previous proto change

--- ResultMicroservice/grpcServers/resultServer.go
+++ ResultMicroservice/grpcServers/resultServer.go
@@ -1,26 +1,58 @@
 package grpcServers
 
 import (
-	"ResultMicroservice/proto/result"
+	"ResultMicroservice/proto/result_pb"
 	"context"
 )
 
 type ResultServer struct {
-	result.UnimplementedGrpcResultServer
+	result_pb.UnimplementedGrpcResultServer
 }
 
-func (s *ResultServer) Delete(ctx context.Context, req *result.ResultRequest) (*result.Result, error) {
-	//ToDO: Implement the Delete RPC
-	result := &result.Result{
-		//ToDo Import service method that uses
+func (s *ResultServer) DeleteByModule(ctx context.Context, req *result_pb.DeleteByModuleRequest) (*result_pb.Result, error) {
+	// TODO: Implement the DeleteByModule RPC
+	result := &result_pb.Result{
+		// TODO: Call the service method that uses req.ModuleID and req.BearerToken
 	}
 	return result, nil
 }
 
-func (s *ResultServer) SoftDelete(ctx context.Context, req *result.ResultRequest) (*result.Result, error) {
-	//ToDO: Implement the SoftDelete RPC
-	result := &result.Result{
-		//ToDo Import service method that uses
+func (s *ResultServer) SoftDeleteByModule(ctx context.Context, req *result_pb.DeleteByModuleRequest) (*result_pb.Result, error) {
+	// TODO: Implement the SoftDeleteByModule RPC
+	result := &result_pb.Result{
+		// TODO: Call the service method that uses req.ModuleID and req.BearerToken
+	}
+	return result, nil
+}
+
+func (s *ResultServer) DeleteByClass(ctx context.Context, req *result_pb.DeleteByClassRequest) (*result_pb.Result, error) {
+	// TODO: Implement the DeleteByClass RPC
+	result := &result_pb.Result{
+		// TODO: Call the service method that uses req.ClassID and req.BearerToken
+	}
+	return result, nil
+}
+
+func (s *ResultServer) SoftDeleteByClass(ctx context.Context, req *result_pb.DeleteByClassRequest) (*result_pb.Result, error) {
+	// TODO: Implement the SoftDeleteByClass RPC
+	result := &result_pb.Result{
+		// TODO: Call the service method that uses req.ClassID and req.BearerToken
+	}
+	return result, nil
+}
+
+func (s *ResultServer) DeleteByUser(ctx context.Context, req *result_pb.DeleteByUserRequest) (*result_pb.Result, error) {
+	// TODO: Implement the DeleteByUser RPC
+	result := &result_pb.Result{
+		// TODO: Call the service method that uses req.UserID and req.BearerToken
+	}
+	return result, nil
+}
+
+func (s *ResultServer) SoftDeleteByUser(ctx context.Context, req *result_pb.DeleteByUserRequest) (*result_pb.Result, error) {
+	// TODO: Implement the SoftDeleteByUser RPC
+	result := &result_pb.Result{
+		// TODO: Call the service method that uses req.UserID and req.BearerToken
 	}
 	return result, nil
 }
