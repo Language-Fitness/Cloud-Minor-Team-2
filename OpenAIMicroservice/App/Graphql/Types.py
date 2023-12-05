eadc42fc9c47d8f72d333be0acced198515ed696
Bastiaan van der Bijl <666669@student.inholland.nl>
2023-12-05 11:16:06 +0100

    pim pam pet

--- OpenAIMicroservice/App/Graphql/Types.py
+++ OpenAIMicroservice/App/Graphql/Types.py
@@ -1,33 +1,33 @@
 import graphene
 
 class MultipleChoiceQuestion(graphene.ObjectType):
-    type = graphene.String()
-    question_info = graphene.String()
-    question_text = graphene.String()
-    options = graphene.List(graphene.String)
-    answer = graphene.String()
+    type = graphene.String(description="Type question")
+    question_info = graphene.String(description="Question info")
+    question_text = graphene.String(description="Question text")
+    options = graphene.List(graphene.String, description="Answer options")
+    answer = graphene.String(description="Correct answer")
 
 class ResponseMultipleChoiceQuestion(graphene.ObjectType):
-    status = graphene.String()
-    data = graphene.List(MultipleChoiceQuestion)
+    status = graphene.String(description="Status of the response")
+    questions = graphene.List(MultipleChoiceQuestion, description="Multiple choice array")
 
 class OpenAnswerQuestion(graphene.ObjectType):
-    type = graphene.String()
-    question_info = graphene.String()
-    question_text = graphene.String()
-    answer = graphene.String()
+    type = graphene.String(description="Type question")
+    question_info = graphene.String(description="Question info")
+    question_text = graphene.String(description="Question text")
+    answer = graphene.String(description="Correct answer")
 
 class ResponseOpenAnswerQuestion(graphene.ObjectType):
-    status = graphene.String()
-    questions = graphene.List(OpenAnswerQuestion)
+    status = graphene.String(description="Status of the response")
+    questions = graphene.List(OpenAnswerQuestion, description="Open answer array")
 
 class Explanation(graphene.ObjectType):
-    info = graphene.String()
-    tips = graphene.String()
+    info = graphene.String(description="Information for explanation")
+    tips = graphene.String(description="Tips for next time")
 
 class ResponseExplanation(graphene.ObjectType):
-    status = graphene.String()
-    explanation = graphene.Field(Explanation)
+    status = graphene.String(description="Status of the response")
+    explanation = graphene.Field(Explanation, description="Explanation body")
 
 class ResponseAnswer(graphene.ObjectType):
     status = graphene.String(description="Status of the response")
--- OpenAIMicroservice/App/Services/OpenAI/AssistantAPIAdapter.py
+++ OpenAIMicroservice/App/Services/OpenAI/AssistantAPIAdapter.py
@@ -1,5 +1,7 @@
 import base64
 import json
+import re
+from App.Graphql.Types import ResponseMultipleChoiceQuestion, MultipleChoiceQuestion
 from App.Services.OpenAI.OpenAIAssistantManager import OpenAIAssistantManager
 
 
@@ -12,8 +14,10 @@ class AssistantAPIAdapter:
             "Services/OpenAI/Assistants/OpenAnswerQuestionsAssistant.json")
         assistant = self.assistant_manager.create_assistant(assistant_json)
 
+        request = f"onderwerp: {subject}, aantal vragen: {amount_questions}"
+
         thread = self.assistant_manager.create_thread()
-        messsage = self.assistant_manager.create_message(thread.id, "test")
+        messsage = self.assistant_manager.create_message(thread.id, request)
 
         run = self.assistant_manager.run_thread(thread.id, assistant.id)
         token = self.encode_token(run.thread_id, run.assistant_id)
@@ -24,8 +28,10 @@ class AssistantAPIAdapter:
             "Services/OpenAI/Assistants/MultipleChoiceQuestionAssistant.json")
         assistant = self.assistant_manager.create_assistant(assistant_json)
 
+        request = f"onderwerp: {subject}, aantal vragen: {amount_questions}"
+
         thread = self.assistant_manager.create_thread()
-        messsage = self.assistant_manager.create_message(thread.id, "test")
+        messsage = self.assistant_manager.create_message(thread.id, request)
 
         run = self.assistant_manager.run_thread(thread.id, assistant.id)
         token = self.encode_token(run.thread_id, run.assistant_id)
@@ -57,17 +63,40 @@ class AssistantAPIAdapter:
         thread_id, assistant_id = self.decode_token(token)
 
         messages = self.assistant_manager.retrieve_messages(thread_id)
-        self.assistant_manager.delete_thread(thread_id)
-        self.assistant_manager.delete_assistant(assistant_id)
 
-        return messages
+        json_data = self.get_last_message(messages)
+
+        try:
+            json_data_dict = json.loads(json_data)
+            return ResponseMultipleChoiceQuestion(
+                status=json_data_dict.get("status"),
+                questions=[
+                    MultipleChoiceQuestion(
+                        type=item["type"],
+                        question_info=item["question_info"],
+                        question_text=item["question_text"],
+                        options=item["options"],
+                        answer=item["answer"]
+                    )
+                    for item in json_data_dict["questions"]
+                ]
+            )
+        except json.JSONDecodeError as e:
+            # Handle JSON decoding error
+            print(f"Error decoding JSON: {e}")
+            return None
+
+
+            # self.assistant_manager.delete_thread(thread_id)
+        # self.assistant_manager.delete_assistant(assistant_id)
+
 
     def retrieve_open_answer_questions(self, token):
         thread_id, assistant_id = self.decode_token(token)
 
         messages = self.assistant_manager.retrieve_messages(thread_id)
-        self.assistant_manager.delete_thread(thread_id)
-        self.assistant_manager.delete_assistant(assistant_id)
+        # self.assistant_manager.delete_thread(thread_id)
+        # self.assistant_manager.delete_assistant(assistant_id)
 
         return messages
 
@@ -81,24 +110,67 @@ class AssistantAPIAdapter:
         return messages
 
     def retrieve_answer(self, token):
-        thread_id, assistant_id = self.decode_token(token)
+        try:
+            thread_id, assistant_id = self.decode_token(token)
 
-        messages = self.assistant_manager.retrieve_messages(thread_id)
-        self.assistant_manager.delete_thread(thread_id)
-        self.assistant_manager.delete_assistant(assistant_id)
+            messages = self.assistant_manager.retrieve_messages(thread_id)
+            self.assistant_manager.delete_thread(thread_id)
+            self.assistant_manager.delete_assistant(assistant_id)
 
-        return messages
+            return messages
+
+        except ValueError as e:
+            return {'error': str(e)}
 
     def encode_token(self, thread_id, assistant_id):
-        ids_dict = {'thread_id': thread_id, 'assistant_id': assistant_id}
-        ids_json = json.dumps(ids_dict)
-        ids_bytes = ids_json.encode('utf-8')
-        encoded_ids = base64.b64encode(ids_bytes)
+        try:
+            ids_dict = {'thread_id': thread_id, 'assistant_id': assistant_id}
+            ids_json = json.dumps(ids_dict)
+            ids_bytes = ids_json.encode('utf-8')
+            encoded_ids = base64.b64encode(ids_bytes)
 
-        return encoded_ids.decode('utf-8')
+            return encoded_ids.decode('utf-8')
+        except Exception as e:
+            raise ValueError(f"Token decoding error: {e}")
 
     def decode_token(self, token):
-        ids_bytes = base64.b64decode(token)
-        ids_json = ids_bytes.decode('utf-8')
-        ids_dict = json.loads(ids_json)
-        return ids_dict['thread_id'], ids_dict['assistant_id']
+        if not self.is_valid_base64(token):
+            raise ValueError("Token decoding error: Invalid Base64 encoding")
+
+        try:
+            ids_bytes = base64.b64decode(token)
+            ids_json = ids_bytes.decode('utf-8')
+            ids_dict = json.loads(ids_json)
+
+            return ids_dict['thread_id'], ids_dict['assistant_id']
+        except Exception as e:
+            raise ValueError(f"Token decoding error: {e}")
+
+    def is_valid_base64(self, token):
+        if not token or len(token) % 4 != 0:
+            return False
+
+        if not re.match('^[A-Za-z0-9+/]+={0,2}$', token):
+            return False
+        return True
+
+    def get_last_message(self, messages):
+        if messages and messages.data:
+            last_assistant_message = None
+
+            # Iterate through the messages in reverse order to find the last assistant message.
+            for message in reversed(messages.data):
+                if message.role == 'assistant':
+                    last_assistant_message = message
+                    break
+
+            if last_assistant_message:
+                # Access the content of the last assistant message.
+                last_assistant_message_content = last_assistant_message.content[0].text.value
+
+                # Now you can use `last_assistant_message_content` as the last message said by the assistant.
+                return last_assistant_message_content
+            else:
+                print("No assistant message found in the list of messages.")
+        else:
+            print("The list of messages is empty.")
