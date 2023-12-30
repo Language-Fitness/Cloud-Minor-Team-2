31a06f8720942a6751ea09e6e2012f426f718e4e
Merlijn Busch <merlijn1busch@gmail.com>
2023-12-30 16:26:51 +0100

    code clean up

--- ModuleMicroservice/server.go
+++ ModuleMicroservice/server.go
@@ -22,11 +22,6 @@ import (
 const defaultPort = "8081"
 
 func main() {
-	err := os.Setenv("GODEBUG", "http2debug=1")
-	if err != nil {
-		return
-	}
-
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
@@ -37,62 +32,29 @@ func main() {
 	}
 
 	//migrations.Init()
-
-	// Initialize GraphQL server
 	graphQLServer := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: graph.NewResolver()}))
 
-	// Token middleware
 	tokenMiddleware := auth.Middleware
 
-	// Define your HTTP router
 	r := mux.NewRouter()
 
-	// GraphQL endpoint
 	r.Handle("/query", tokenMiddleware(graphQLServer))
-
-	// GraphQL playground
 	r.Handle("/", playground.Handler("GraphQL playground", "/query"))
-
-	// Metrics endpoint
 	r.Handle("/metrics", promhttp.Handler())
-
-	// Set up CORS middleware if needed
-	// corsMiddleware := handlers.CORS(
-	//     handlers.AllowedOrigins([]string{"*"}),
-	//     handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
-	//     handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
-	// )
-
-	// Create a handler with logging and CORS middleware
 	msHandler := handlers.LoggingHandler(os.Stdout, r)
-	// handler = corsMiddleware(handler)
 
 	go grpcSagaServer()
 
-	//// Start the HTTP server
-	//log.Printf("SagaService is running on http://localhost:%s", port)
-	//log.Fatal(http.ListenAndServe(":"+port, msHandler))
-
-	log.Printf("SagaService is running on http://localhost:%s", defaultPort)
-	log.Fatal(http.ListenAndServe(":"+defaultPort, msHandler))
+	// Start the HTTP server
+	log.Printf("SagaService is running on http://localhost:%s", port)
+	log.Fatal(http.ListenAndServe(":"+port, msHandler))
 }
 
 func grpcSagaServer() {
-	//// Initialize gRPC connection
-	// TODO Dial is something that a grpc client uses to connect to a grpc server, but this seems to be a server
-	//conn, err := grpc.Dial("localhost:9091", grpc.WithInsecure())
-	//if err != nil {
-	//	log.Fatalf("failed to dial gRPC server: %v", err)
-	//}
-	//defer conn.Close()
-
-	lis, err := net.Listen("tcp", ":9091") // Define your gRPC server address and port
+	lis, err := net.Listen("tcp", ":9091")
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
 	}
-
-	// Create a gRPC client using the connection
-	// Create a new gRPC server instance
 
 	grpcServer := grpc.NewServer()
 	collection, _ := database.GetCollection()
