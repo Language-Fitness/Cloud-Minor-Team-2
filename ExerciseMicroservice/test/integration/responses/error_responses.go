f05fde5cb319fe9cdd5b2a723dfea52c09d984d5
Jaimy Monsuur <jaimymonsuur@gmail.com>
2024-01-12 16:52:05 +0100

    added more tests

--- ExerciseMicroservice/test/integration/responses/error_responses.go
+++ ExerciseMicroservice/test/integration/responses/error_responses.go
@@ -7,6 +7,9 @@ var InvalidQuestionResponseError = "Validation errors: Question length should be
 var NoCorrectAnswersResponseError = "at least one answer must be correct"
 var NoIncorrectAnswersResponseError = "only one answer can be correct"
 var NoAnswersResponseError = "exercise must have at least two answers"
+var ExerciseNotFoundResponseError = "exercise not found"
+var GetExerciseInvalidUUIDResponseError = "Validation errors: ID :'95f964a0-9749-4064-9162-cdd1b7b5d76' is not a valid UUID"
+var InvalidPermissionResponseError = "invalid permissions for this action"
 
 type ErrorType struct {
 	Message string   `json:"message"`
--- ExerciseMicroservice/test/integration/responses/exercise_responses.go
+++ ExerciseMicroservice/test/integration/responses/exercise_responses.go
@@ -31,3 +31,19 @@ var UpdateExerciseResponse struct {
 		MadeBy     string `json:"made_by"`
 	}
 }
+
+var GetExerciseResponse struct {
+	GetExercise struct {
+		ID       string
+		ClassID  string `json:"class_Id"`
+		ModuleID string `json:"module_id"`
+		Name     string
+		Question string
+		Answers  []struct {
+			Value   string
+			Correct bool
+		}
+		Difficulty string
+		MadeBy     string `json:"made_by"`
+	}
+}
