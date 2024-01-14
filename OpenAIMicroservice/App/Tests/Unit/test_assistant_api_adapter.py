f59af128300d293bd9ca47ee89c0f109a7b2f161
Bastiaan van der Bijl <666669@student.inholland.nl>
2024-01-14 14:45:13 +0100

    pew pew

--- OpenAIMicroservice/App/Tests/Unit/test_assistant_api_adapter.py
+++ OpenAIMicroservice/App/Tests/Unit/test_assistant_api_adapter.py
@@ -76,7 +76,7 @@ class TestAssistantAPIAdapter(unittest.TestCase):
 
     # 1.1 Failure in Loading Assistant
     @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.load_assistant')
-    def test_load_assistant_failure_exception(self, mock_load_assistant):
+    def test_generate_multiple_choice__load_assistant_failure_exception(self, mock_load_assistant):
         mock_load_assistant.side_effect = Exception("Load assistant failure")
         with self.assertRaises(Exception):
             self.assistantAPIAdapter.generate_multiple_choice_questions("Subject", "Level", 5)
@@ -84,7 +84,7 @@ class TestAssistantAPIAdapter(unittest.TestCase):
     # 1.2 Failure in Creating Assistant (Exception)
     @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.load_assistant')
     @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_assistant')
-    def test_create_assistant_failure_exception(self, mock_create_assistant, mock_load_assistant):
+    def test_generate_multiple_choice__create_assistant_failure_exception(self, mock_create_assistant, mock_load_assistant):
         mock_load_assistant.return_value = MagicMock()
         mock_create_assistant.side_effect = Exception(
             "Message could not be created. check fields json fields in assistant json.")
@@ -94,7 +94,7 @@ class TestAssistantAPIAdapter(unittest.TestCase):
     # 1.3 Failure in Creating Assistant (AssistantAPIException)
     @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.load_assistant')
     @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_assistant')
-    def test_create_assistant_failure_assistant_api_exception(self, mock_create_assistant, mock_load_assistant):
+    def test_generate_multiple_choice__create_assistant_failure_assistant_api_exception(self, mock_create_assistant, mock_load_assistant):
         mock_load_assistant.return_value = MagicMock()
         mock_create_assistant.side_effect = AssistantAPIException(
             "Request could not be fulfilled. Please provide an valid OpenAI API key in CMS.")
@@ -116,7 +116,7 @@ class TestAssistantAPIAdapter(unittest.TestCase):
     @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.load_assistant')
     @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_assistant')
     @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_thread')
-    def test_create_thread_failure_assistant_api_exception(self, mock_create_thread, mock_create_assistant,
+    def test_generate_multiple_choice__create_thread_failure_assistant_api_exception(self, mock_create_thread, mock_create_assistant,
                                                            mock_load_assistant):
         mock_load_assistant.return_value = MagicMock()
         mock_create_assistant.return_value = MagicMock()
@@ -130,7 +130,7 @@ class TestAssistantAPIAdapter(unittest.TestCase):
     @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_assistant')
     @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_thread')
     @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_message')
-    def test_create_message_failure_assistant_api_exception(self, mock_create_message, mock_create_thread,
+    def test_generate_multiple_choice__create_message_failure_assistant_api_exception(self, mock_create_message, mock_create_thread,
                                                             mock_create_assistant, mock_load_assistant):
         mock_load_assistant.return_value = MagicMock()
         mock_create_assistant.return_value = MagicMock()
@@ -145,7 +145,7 @@ class TestAssistantAPIAdapter(unittest.TestCase):
     @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_assistant')
     @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_thread')
     @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_message')
-    def test_create_message_failure_exception(self, mock_create_message, mock_create_thread, mock_create_assistant,
+    def test_generate_multiple_choice__create_message_failure_exception(self, mock_create_message, mock_create_thread, mock_create_assistant,
                                               mock_load_assistant):
         mock_load_assistant.return_value = MagicMock()
         mock_create_assistant.return_value = MagicMock()
@@ -160,7 +160,7 @@ class TestAssistantAPIAdapter(unittest.TestCase):
     @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_thread')
     @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_message')
     @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.run_thread')
-    def test_run_thread_failure_assistant_api_exception(self, mock_run_thread, mock_create_message, mock_create_thread,
+    def test_generate_multiple_choice__run_thread_failure_assistant_api_exception(self, mock_run_thread, mock_create_message, mock_create_thread,
                                                         mock_create_assistant, mock_load_assistant):
         mock_load_assistant.return_value = MagicMock()
         mock_create_assistant.return_value = MagicMock()
@@ -177,7 +177,7 @@ class TestAssistantAPIAdapter(unittest.TestCase):
     @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_thread')
     @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_message')
     @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.run_thread')
-    def test_run_thread_failure_exception(self, mock_run_thread, mock_create_message, mock_create_thread,
+    def test_generate_multiple_choice__run_thread_failure_exception(self, mock_run_thread, mock_create_message, mock_create_thread,
                                           mock_create_assistant, mock_load_assistant):
         mock_load_assistant.return_value = MagicMock()
         mock_create_assistant.return_value = MagicMock()
@@ -194,7 +194,7 @@ class TestAssistantAPIAdapter(unittest.TestCase):
     @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_message')
     @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.run_thread')
     @patch('services.openai.assistant_api_adapter.AssistantAPIAdapter.encode_token')
-    def test_encode_token_failure(self, mock_encode_token, mock_run_thread, mock_create_message, mock_create_thread,
+    def test_generate_multiple_choice__encode_token_failure(self, mock_encode_token, mock_run_thread, mock_create_message, mock_create_thread,
                                   mock_create_assistant, mock_load_assistant):
         mock_load_assistant.return_value = MagicMock()
         mock_create_assistant.return_value = MagicMock()
@@ -262,8 +262,142 @@ class TestAssistantAPIAdapter(unittest.TestCase):
 
     # FAIL TESTS
 
-    # 1.1 Failure in Loading Assistant
+    # 2.1 Failure in Loading Assistant
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.load_assistant')
+    def test_generate_explanation__load_assistant_failure_exception(self, mock_load_assistant):
+        mock_load_assistant.side_effect = Exception("Load assistant failure")
+        with self.assertRaises(Exception):
+            self.assistantAPIAdapter.generate_explanation("Woordenschat", "wat doe je als je ‘een oogje in het zeil houdt’?", "je bent op zoek naar een specifiek zeilschip", "je let goed op om te zorgen dat alles goed gaat")
 
+    # 2.2 Failure in Creating Assistant (Exception)
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.load_assistant')
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_assistant')
+    def test_generate_explanation__create_assistant_failure_exception(self, mock_create_assistant, mock_load_assistant):
+        mock_load_assistant.return_value = MagicMock()
+        mock_create_assistant.side_effect = Exception(
+            "Message could not be created. check fields json fields in assistant json.")
+        with self.assertRaises(Exception):
+            self.assistantAPIAdapter.generate_explanation("Woordenschat", "wat doe je als je ‘een oogje in het zeil houdt’?", "je bent op zoek naar een specifiek zeilschip", "je let goed op om te zorgen dat alles goed gaat")
 
+    # 2.3 Failure in Creating Assistant (AssistantAPIException)
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.load_assistant')
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_assistant')
+    def test_generate_explanation__create_assistant_failure_assistant_api_exception(self, mock_create_assistant, mock_load_assistant):
+        mock_load_assistant.return_value = MagicMock()
+        mock_create_assistant.side_effect = AssistantAPIException(
+            "Request could not be fulfilled. Please provide an valid OpenAI API key in CMS.")
+        with self.assertRaises(AssistantAPIException):
+            self.assistantAPIAdapter.generate_explanation("Woordenschat", "wat doe je als je ‘een oogje in het zeil houdt’?", "je bent op zoek naar een specifiek zeilschip", "je let goed op om te zorgen dat alles goed gaat")
+
+    # 2.4 Failure in Creating Thread (Exception)
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.load_assistant')
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_assistant')
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_thread')
+    def test_generate_explanation__create_thread_failure_exception(self, mock_create_thread, mock_create_assistant, mock_load_assistant):
+        mock_load_assistant.return_value = MagicMock()
+        mock_create_assistant.return_value = MagicMock()
+        mock_create_thread.side_effect = Exception("An unexpected error occurred while creating thread.")
+        with self.assertRaises(Exception):
+            self.assistantAPIAdapter.generate_explanation("Woordenschat", "wat doe je als je ‘een oogje in het zeil houdt’?", "je bent op zoek naar een specifiek zeilschip", "je let goed op om te zorgen dat alles goed gaat")
+
+    # 2.5 Failure in Creating Thread (AssistantAPIException)
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.load_assistant')
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_assistant')
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_thread')
+    def test_generate_explanation__create_thread_failure_assistant_api_exception(self, mock_create_thread, mock_create_assistant,
+                                                           mock_load_assistant):
+        mock_load_assistant.return_value = MagicMock()
+        mock_create_assistant.return_value = MagicMock()
+        mock_create_thread.side_effect = AssistantAPIException(
+            "Request could not be fulfilled. Please provide an valid OpenAI API key in CMS.")
+        with self.assertRaises(AssistantAPIException):
+            self.assistantAPIAdapter.generate_explanation("Woordenschat", "wat doe je als je ‘een oogje in het zeil houdt’?", "je bent op zoek naar een specifiek zeilschip", "je let goed op om te zorgen dat alles goed gaat")
+
+    # 2.6 Failure in Creating Message(AssistantAPIException)
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.load_assistant')
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_assistant')
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_thread')
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_message')
+    def test_generate_explanation__create_message_failure_assistant_api_exception(self, mock_create_message, mock_create_thread,
+                                                            mock_create_assistant, mock_load_assistant):
+        mock_load_assistant.return_value = MagicMock()
+        mock_create_assistant.return_value = MagicMock()
+        mock_create_thread.return_value = MagicMock()
+        mock_create_message.side_effect = AssistantAPIException(
+            "Request could not be fulfilled. Please provide an valid OpenAI API key in CMS.")
+        with self.assertRaises(AssistantAPIException):
+            self.assistantAPIAdapter.generate_explanation("Woordenschat", "wat doe je als je ‘een oogje in het zeil houdt’?", "je bent op zoek naar een specifiek zeilschip", "je let goed op om te zorgen dat alles goed gaat")
+
+    # 2.7 Failure in Creating Message(Exception)
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.load_assistant')
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_assistant')
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_thread')
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_message')
+    def test_generate_explanation__create_message_failure_exception(self, mock_create_message, mock_create_thread, mock_create_assistant,
+                                              mock_load_assistant):
+        mock_load_assistant.return_value = MagicMock()
+        mock_create_assistant.return_value = MagicMock()
+        mock_create_thread.return_value = MagicMock()
+        mock_create_message.side_effect = Exception("Message could not be created. check thread_id and message.")
+        with self.assertRaises(Exception):
+            self.assistantAPIAdapter.generate_explanation("Woordenschat", "wat doe je als je ‘een oogje in het zeil houdt’?", "je bent op zoek naar een specifiek zeilschip", "je let goed op om te zorgen dat alles goed gaat")
+
+    # 2.8 Failure in Running Thread(AssistantAPIException)
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.load_assistant')
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_assistant')
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_thread')
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_message')
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.run_thread')
+    def test_generate_explanation__run_thread_failure_assistant_api_exception(self, mock_run_thread, mock_create_message, mock_create_thread,
+                                                        mock_create_assistant, mock_load_assistant):
+        mock_load_assistant.return_value = MagicMock()
+        mock_create_assistant.return_value = MagicMock()
+        mock_create_thread.return_value = MagicMock()
+        mock_create_message.return_value = MagicMock()
+        mock_run_thread.side_effect = AssistantAPIException(
+            "Request could not be fulfilled. Please provide an valid OpenAI API key in CMS.")
+        with self.assertRaises(AssistantAPIException):
+            self.assistantAPIAdapter.generate_explanation("Woordenschat", "wat doe je als je ‘een oogje in het zeil houdt’?", "je bent op zoek naar een specifiek zeilschip", "je let goed op om te zorgen dat alles goed gaat")
+
+    # 2.9 Failure in Running Thread
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.load_assistant')
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_assistant')
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_thread')
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_message')
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.run_thread')
+    def test_generate_explanation__run_thread_failure_exception(self, mock_run_thread, mock_create_message, mock_create_thread,
+                                          mock_create_assistant, mock_load_assistant):
+        mock_load_assistant.return_value = MagicMock()
+        mock_create_assistant.return_value = MagicMock()
+        mock_create_thread.return_value = MagicMock()
+        mock_create_message.return_value = MagicMock()
+        mock_run_thread.side_effect = Exception("Thread could not be run. check thread_id and assistant_id.")
+        with self.assertRaises(Exception):
+            self.assistantAPIAdapter.generate_explanation("Woordenschat", "wat doe je als je ‘een oogje in het zeil houdt’?", "je bent op zoek naar een specifiek zeilschip", "je let goed op om te zorgen dat alles goed gaat")
+
+    # 2.10 Failure in Token Encoding
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.load_assistant')
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_assistant')
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_thread')
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.create_message')
+    @patch('services.openai.openai_assistant_manager.OpenAIAssistantManager.run_thread')
+    @patch('services.openai.assistant_api_adapter.AssistantAPIAdapter.encode_token')
+    def test_generate_explanation__encode_token_failure(self, mock_encode_token, mock_run_thread, mock_create_message, mock_create_thread,
+                                  mock_create_assistant, mock_load_assistant):
+        mock_load_assistant.return_value = MagicMock()
+        mock_create_assistant.return_value = MagicMock()
+        mock_create_thread.return_value = MagicMock()
+        mock_create_message.return_value = MagicMock()
+        mock_run_thread.return_value = MagicMock()
+
+        mock_encode_token.side_effect = AssistantAPIException("Token encoding error")
+        with self.assertRaises(AssistantAPIException):
+            self.assistantAPIAdapter.generate_explanation("Woordenschat", "wat doe je als je ‘een oogje in het zeil houdt’?", "je bent op zoek naar een specifiek zeilschip", "je let goed op om te zorgen dat alles goed gaat")
+
+    # ALL TESTS FOR: RETRIEVE_RESPONSE
+
+    # SUCCESS TESTS
+
+    # 3. Successfully running the method
 if __name__ == '__main__':
     unittest.main()
